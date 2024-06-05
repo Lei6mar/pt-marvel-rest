e09133f4a81f5775d1f636491d7dda3f32d13458
Mario Ledesma <miledesmam@gmail.com>
2024-06-04 18:20:25 -0600

    Se agrega lectura de variable Api_url desde properties

--- src/main/java/mx/com/bwl/marvel_rest/service/MarvelService.java
+++ src/main/java/mx/com/bwl/marvel_rest/service/MarvelService.java
@@ -1,18 +1,20 @@
 package mx.com.bwl.marvel_rest.service;
 
 import mx.com.bwl.marvel_api.service.impl.MarvelApiServiceImpl;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MarvelService {
 
-    @Value("${API_PUBLIC_KEY}")
-    private String apiPublicKey;
-    @Value("${API_PRIVATE_KEY}")
-    private String apiPrivateKey;
-    @Value("${API_TIMESTAMP_VALUE}")
-    private String apiTimestampValue;
+    private final String apiPublicKey;
+    private final String apiPrivateKey;
+    private final String apiTimestampValue;
+
+    MarvelService(){
+        apiPublicKey = System.getenv("API_PUBLIC");
+        apiPrivateKey = System.getenv("API_PRIVATE");
+        apiTimestampValue = System.getenv("API_TIMESTAMP");
+    }
 
     public mx.com.bwl.marvel_api.vo.marvel.ResponseVO findAllCharacters(){
         mx.com.bwl.marvel_api.service.MarvelApiService marvelApiService = new MarvelApiServiceImpl(apiPublicKey, apiPrivateKey, apiTimestampValue);
